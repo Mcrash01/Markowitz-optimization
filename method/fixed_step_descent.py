a324a090ad59f91f09eb3fce344827506f20a473
Maxlmns <132375358+Maxlmns@users.noreply.github.com>
2023-12-05 17:44:32 +0100

    descente 1

--- method/fixed_step_descent.py
+++ method/fixed_step_descent.py
@@ -1,9 +1,17 @@
-def descente1(grad_f,x0,e,p):
+import numpy as np
+
+def f(w,covariance_matrix):
+  return np.matmul(np.matmul(np.transpose(w),covariance_matrix),w)
+
+def grad_f(w,covariance_matrix):
+  return np.matmul(2*np.transpose(w),covariance_matrix)
+
+def descente_markowitz(x0,covariance_matrix,e,p):
   k = 0
   ek = 2*e
-  while ek>=e:
-    wk = -1*grad_f(x0)
-    ek = (x0+p*wk-x0).norm()
+  while ek>=e :
+    wk = -1*grad_f(x0,covariance_matrix)
+    ek = np.linalg.norm(x0+p*wk-x0)
     x0 = x0+p*wk
     k+=1
-  return x0,k
+  return x0
